b4fce4758260de3a2830998b9dba2d3ee859a237
Milena Maeda <milenaminekawa@gmail.com>
2023-01-14 17:39:10 -0300

    Rails_Task_Manager_Challenge_Completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Create the task
+  get 'new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # Update the task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Read the tasks
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # Destroy
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
