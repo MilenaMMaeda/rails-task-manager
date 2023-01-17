bce096f53c2c783dca19f7b9f9c737dd2b558b7e
Milena Maeda <milenaminekawa@gmail.com>
2023-01-17 17:53:00 -0300

    Rails_Task_Manager_Refactored

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,17 @@
 class TasksController < ApplicationController
+
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
     @task = Task.new
   end
-
 
   def create
     @task = Task.new(form_params)
@@ -19,18 +20,15 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(form_params)
 
     redirect_to tasks_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     redirect_to tasks_path, status: :see_other
@@ -40,5 +38,9 @@ class TasksController < ApplicationController
 
   def form_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,19 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   # Create the task
-  get 'new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
+  # get 'new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
 
-  # Update the task
-  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
-  patch 'tasks/:id', to: 'tasks#update'
+  # # Update the task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  # patch 'tasks/:id', to: 'tasks#update'
 
-  # Read the tasks
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # # Read the tasks
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'
 
-  # Destroy
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # # Destroy
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
